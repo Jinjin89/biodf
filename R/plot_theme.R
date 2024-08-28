b625a2934fcdfa4d3f2de00772d6257c626c8ac0
whoCares <sunkeke1993@gmail.com>
2024-08-28 17:11:44 +0800

    add some theme

--- R/plot_theme.R
+++ R/plot_theme.R
@@ -9,10 +9,15 @@
 tn = function(...){
   ggplot2::theme_minimal()+
     ggplot2::theme(
+      text = ggplot2::element_text(family = 'Arial'),
       panel.grid = ggplot2::element_blank(),
-      axis.line = ggplot2::element_line(),
-      axis.ticks = ggplot2::element_line())+
-    ggplot2::theme(...)
+      #plot.background = ggplot2::element_blank(),
+      plot.background = ggplot2::element_rect(fill = NA,colour = NA),
+      legend.background = ggplot2::element_blank(),
+      panel.background = ggplot2::element_rect(),
+      axis.line = ggplot2::element_blank(),
+      axis.ticks = ggplot2::element_line(),
+      ...)
 }
 
 #' theme for barplot
@@ -24,9 +29,10 @@ tn = function(...){
 #' @return theme obj
 #' @export
 #'
-tn_bar = function(show_axis_title=F,legend_text = 6,...){
+tn_bar = function(show_axis_title=F,show_legend_title=F,legend_text = 6,...){
   ggplot2::theme_minimal()+
     ggplot2::theme(
+      text = ggplot2::element_text(family = 'Arial'),
       panel.grid = ggplot2::element_blank(),
       panel.background = ggplot2::element_rect(fill =NA,color = "black"),
       axis.ticks = ggplot2::element_line(color = "black"),
@@ -34,15 +40,20 @@ tn_bar = function(show_axis_title=F,legend_text = 6,...){
       legend.title = ggplot2::element_blank(),
       legend.text = ggplot2::element_text(size = legend_text),
       legend.key.size = ggplot2::unit(4,"mm"),
-      legend.position = "top"
-    )+
-    ggplot2::theme(...) -> tn
+      legend.position = "top",
+      ...
+    ) -> tn
 
   if(show_axis_title){
     tn =
       tn + ggplot2::theme(
         axis.title = element_text()
       )
+  }
+  if(show_legend_title){
+    tn = tn+ggplot2::theme(
+      legend.title = ggplot2::element_text()
+    )
   }
   tn
 }
@@ -72,3 +83,68 @@ tn_empty <- function(...){
     theme(panel.grid = element_blank())+
     theme(...)
 }
+
+
+
+#' add arial theme
+#'
+#' @param ... passing to ggplot2::theme
+
+#' @return ggplot2 theme obj
+#'
+tn_arial <- function(...){
+  ggplot2::theme(
+    text = ggplot2::element_text(family = 'Arial'),
+    ...
+  )
+}
+
+#' italic the text x
+#'
+#' @param ...  passing to element_text
+#'
+#' @return ggplot2 theme
+#' @export
+#'
+tn_italic_text_x <- function(...){
+  ggplot2::theme(
+    axis.text.x = ggplot2::element_text(face = 'italic',...)
+  )
+}
+
+
+#' italic the text y
+#'
+#' @param ...  passing to element_text
+#'
+#' @return ggplot2 theme
+#'
+#'
+tn_italic_text_y <- function(...){
+  ggplot2::theme(
+    axis.text.y= ggplot2::element_text(face = 'italic',...)
+  )
+  }
+
+#' italic the title x
+#'
+#' @param ...  passing to element_text
+#'
+#' @return ggplot2 theme
+#'
+tn_italic_title_x <- function(...){
+  ggplot2::theme(
+    axis.title.x = ggplot2::element_text(face = 'italic',...)
+  )
+}
+
+#' italic the title y
+#'
+#' @param ...  passing to element_text
+#'
+#' @return ggplot2 theme
+#'
+tn_italic_title_y <- function(...){
+  ggplot2::theme(
+    axis.title.y= ggplot2::element_text(face = 'italic',...)
+  )}
